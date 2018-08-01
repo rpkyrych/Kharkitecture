7c1f55a6349404563ddb094af7e5357a5f98098f
VladyslavSolo <solovladik@mail.ru>
2018-08-01 16:39:08 +0300

    Implementation of the functionality: adding photos to the database

--- Backoffice/src/main/java/com/kharkitecture/backoffice/entity/Photo.java
+++ Backoffice/src/main/java/com/kharkitecture/backoffice/entity/Photo.java
@@ -1,6 +1,7 @@
 package com.kharkitecture.backoffice.entity;
 
 import javax.persistence.*;
+import java.util.Arrays;
 import java.util.Objects;
 
 @Entity
@@ -9,20 +10,58 @@ public class Photo {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
-    private String image;
     @ManyToOne(targetEntity = Building.class)
     private Building building;
+    @Column(name = "original_size")
+    byte[] originalSize;
+    @Column(name = "small_size")
+    byte[] smallSize;
+    @Column(name = "middle_size")
+    byte[] middleSize;
+    @Column(name = "large_size")
+    byte[] largeSize;
 
     public Photo() {
     }
-
-    public Photo(String image) {
-        this.image = image;
+    public Photo(Building building, byte[] originalSize) {
+        this.building = building;
+        this.originalSize = originalSize;
     }
 
-    public Photo(String image, Building building) {
-        this.image = image;
-        this.building = building;
+    public Photo(byte[] originalSize) {
+        this.originalSize = originalSize;
+    }
+
+    public byte[] getOriginalSize() {
+        return originalSize;
+    }
+
+    public void setOriginalSize(byte[] originalSize) {
+        this.originalSize = originalSize;
+    }
+
+    public byte[] getSmallSize() {
+        return smallSize;
+    }
+
+    public void setSmallSize(byte[] smallSize) {
+        this.smallSize = smallSize;
+    }
+
+    public byte[] getMiddleSize() {
+        return middleSize;
+    }
+
+    public void setMiddleSize(byte[] middleSize) {
+        this.middleSize = middleSize;
+    }
+
+    public byte[] getLargeSize() {
+        return largeSize;
+    }
+
+    public void setLargeSize(byte[] largeSize) {
+        this.largeSize = largeSize;
     }
 
     public long getId() {
@@ -31,14 +70,6 @@ public class Photo {
 
     public void setId(long id) {
         this.id = id;
-    }
-
-    public String getImage() {
-        return image;
-    }
-
-    public void setImage(String image) {
-        this.image = image;
     }
 
     public Building getBuilding() {
@@ -52,15 +83,17 @@ public class Photo {
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
-        if (!(o instanceof Photo)) return false;
+        if (o == null || getClass() != o.getClass()) return false;
         Photo photo = (Photo) o;
         return id == photo.id &&
-                Objects.equals(image, photo.image);
+                Arrays.equals(originalSize, photo.originalSize);
     }
 
     @Override
     public int hashCode() {
 
-        return Objects.hash(id, image);
+        int result = Objects.hash(id);
+        result = 31 * result + Arrays.hashCode(originalSize);
+        return result;
     }
 }
