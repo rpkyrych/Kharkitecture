fb5084acd18c0bdf32045c60ea64e92712d07a9b
Roman Kyrychenko <rp.kyrychenko@gmail.com>
2018-08-16 13:28:24 +0300

    KHARKITEC-35: Fixed bug with displaying thumbnail of building image.

--- Backoffice/src/main/java/com/kharkitecture/backoffice/controller/BuildingController.java
+++ Backoffice/src/main/java/com/kharkitecture/backoffice/controller/BuildingController.java
@@ -4,6 +4,10 @@ import com.kharkitecture.backoffice.entity.Building;
 import com.kharkitecture.backoffice.service.BuildingService;
 import org.apache.logging.log4j.*;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ModelAttribute;
@@ -11,6 +15,10 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.servlet.ModelAndView;
+
+import java.io.IOException;
+import java.util.List;
 
 import static org.springframework.web.bind.annotation.RequestMethod.*;
 
@@ -36,9 +44,19 @@ public class BuildingController {
     }
 
     @RequestMapping(value = "/all", method = GET)
-    public String getBuildingsList(Model model) {
-        model.addAttribute("buildings", buildingService.getBuildings());
-        return VIEW_ALL_BUILDINGS_PAGE;
+    public ModelAndView getBuildingsList() {
+        ModelAndView modelAndView = new ModelAndView(VIEW_ALL_BUILDINGS_PAGE);
+        List<Building> buildings = buildingService.getBuildings();
+        modelAndView.addObject("buildings", buildings);
+        return modelAndView;
+    }
+
+    @RequestMapping(value = "/image/{id}", produces = MediaType.IMAGE_JPEG_VALUE)
+    public ResponseEntity<byte[]> getImage(@PathVariable("id") Long buildingId) throws ClassNotFoundException {
+        byte[] imageContent = buildingService.getBuilding(buildingId).getPhotos().get(0).getSmallSize();
+        final HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.IMAGE_JPEG);
+        return new ResponseEntity<>(imageContent, headers, HttpStatus.OK);
     }
 
     @RequestMapping(value = "/{id}", method = GET)
